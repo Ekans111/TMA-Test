233a1c9e0505ee3567258692c5c6427b0ded60ad
ekans111 <kobayasitorao111@gmail.com>
2024-06-08 08:48:22 +0900

    update url

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <TonConnectUIProvider manifestUrl="https://ekans111.github.io/TWA-test/tonconnect-manifest.json">
+    <TonConnectUIProvider manifestUrl="https://ekans111.github.io/TMA-Test/tonconnect-manifest.json">
       <App />
     </TonConnectUIProvider>
   </React.StrictMode>
