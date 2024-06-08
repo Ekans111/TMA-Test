779a4632a45356aa64eb25b1cb59887e07f67420
ekans111 <kobayasitorao111@gmail.com>
2024-06-08 12:28:50 +0900

    resize

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ function App() {
             className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center rounded-full cursor-pointer transition hover:scale-110 hover:opacity-50 active:opacity-80"
             onClick={() => setTon(ton + 1)}
           >
-            <img src="/TMA-Test/image/toncoin.svg" alt="toncoin" className="w-40 h-40 max-sm:w-30 max-sm:h-30" />{" "}
+            <img src="/TMA-Test/image/toncoin.svg" alt="toncoin" className="w-40 h-40 max-sm:w-20 max-sm:h-20" />{" "}
           </div>
           <section className="tasks absolute bottom-4 left-1/2 -translate-x-1/2">
             <h2 className="text-2xl mb-5">
